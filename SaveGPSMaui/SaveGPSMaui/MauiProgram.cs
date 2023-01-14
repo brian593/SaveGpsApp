bcc57248ad50b60edc106731b9fe7cf2bc552405
brianmolinabastidas <brian593@live.com>
2023-01-14 11:33:43 -0500

    only android platform

--- SaveGPSMaui/SaveGPSMaui/MauiProgram.cs
+++ SaveGPSMaui/SaveGPSMaui/MauiProgram.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui;
+
 
 namespace SaveGPSMaui;
 
@@ -9,11 +11,18 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
+             .UseSentry(options =>
+             {
+                 options.Dsn = "https://a9b06ae04a234bee9568f5a296e87a3a@o4504503705337856.ingest.sentry.io/4504503732994048";
+                 options.Debug = true;
+                 options.TracesSampleRate = 1.0;
+             })
+            .ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+			})
+            .UseMauiCommunityToolkit();
 
 #if DEBUG
 		builder.Logging.AddDebug();
